8dfd6152325e80912063f512bb41c2ad899c4c96
Trpevski <milo.trpevski@accenture.com>
2023-01-24 18:30:13 +0100

    add first components and configured ngx-spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,38 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DialogModule } from 'primeng/dialog';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
+// import { DialogModule } from 'primeng/dialog';
+// import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { MomentModule } from 'ngx-moment';
+// import { MomentModule } from 'ngx-moment';
 import { HomeComponent } from './pages/home/home.component';
 import { CasualExpensesComponent } from './pages/casual-expenses/casual-expenses.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, CasualExpensesComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CasualExpensesComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    DialogModule,
-    DynamicDialogModule,
+    // DialogModule,
+    // DynamicDialogModule,
+    BrowserAnimationsModule,
     NgxSpinnerModule,
-    MomentModule,
+    // MomentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
