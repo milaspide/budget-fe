6140d4163b3aff78f6d561e4df79aa0a0055b64b
milaspide <er_miletto@yahoo.it>
2023-01-25 22:50:34 +0100

    fix primeng version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { DialogModule } from 'primeng/dialog';
-// import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { NgxSpinnerModule } from 'ngx-spinner';
-// import { MomentModule } from 'ngx-moment';
 import { HomeComponent } from './pages/home/home.component';
 import { CasualExpensesComponent } from './pages/casual-expenses/casual-expenses.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
@@ -25,11 +24,10 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    // DialogModule,
-    // DynamicDialogModule,
+    DialogModule,
+    DynamicDialogModule,
     BrowserAnimationsModule,
     NgxSpinnerModule,
-    // MomentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
